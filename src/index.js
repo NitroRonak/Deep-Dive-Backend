ef543e6d57d850f33110f5b688e899dd17680aae
NitroRonak <ronaks0988@gmail.com>
2023-11-07 23:02:56 +0530

    custom apiResponse and Error handler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import dotenv from 'dotenv'
 import {connectDB} from './db/index.js'
+import {app} from "./app.js"
 dotenv.config();
-connectDB()
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Server started on port ${process.env.PORT}`);
+    });
+}).catch(err=>{
+    console.log("Mongo connection failed: " + err.message);
+});
