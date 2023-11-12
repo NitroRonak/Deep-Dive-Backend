ee3aa64510bd6e0a7cfef75fda9b653f16257c9c
NitroRonak <ronaks0988@gmail.com>
2023-11-12 10:50:09 +0530

    add cloudinary upload functionalitiy

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,23 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadCloudinary = async (filePathUrl)=>{
+    try{
+        if(!filePathUrl) return null;
+        const res=await cloudinary.uploader.upload(filePathUrl,{resource_type:'auto'});
+        console.log('file is uploaded successfully '+ res.url);
+        return res;
+    }
+    catch(err){
+       fs.unlinkSync(filePathUrl);
+       return null;
+    }
+}
+
+export {uploadCloudinary}
