ca9d46eeea00277a8be87d3a8a65ab325712f043
NitroRonak <ronaks0988@gmail.com>
2023-12-16 17:59:52 +0530

    login controller done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,15 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { uploadCloudinary } from "../utils/Cloudinary.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+const generateAccessAndRefreshToken = async (userID)=>{
+    const user=await User.findById(userID);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({validateBeforeSave:false});
+    return {accessToken,refreshToken}
+
+}
 export const registerUser=asyncHandler(async (req,res)=>{
     const {username,password,email,fullName}=req.body;
     if([username,password,email,fullName].some((field)=>
@@ -50,4 +59,38 @@ export const registerUser=asyncHandler(async (req,res)=>{
     return res.status(201).json(
         new ApiResponse(200, createdUser, "User registered successfully")
     )
+})
+
+export const loginUser=asyncHandler(async (req,res)=>{
+    const {username,email,password}=req.body
+    if(!username || !email){
+        throw new ApiError(400,"username and email does not exist");
+    }
+    const user=User.findOne({
+        $or:[{username},{email}]
+    })
+
+    if(!user){
+        throw new ApiError(404,"user does not exist");
+    }
+
+    const isPasswordValid=await user.comparePassword(password);
+
+    if(!isPasswordValid){
+        throw new ApiError(401,"Invalid credential");
+    }
+    const {accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id);
+    
+    const loggedInUser=User.findById(user._id)
+    .select("-password -refreshToken");
+
+    return res.status(200)
+    .cookie("accessToken",accessToken)
+    .cookie("refreshToken",refreshToken)
+    .json(new ApiResponse(200,{
+        user:loggedInUser , accessToken ,refreshToken
+    },
+    "User Successfully logged In"
+    ))
+
 })
