c7742570e580efebc3e613cf8983fcc80dc8b31f
NitroRonak <ronaks0988@gmail.com>
2023-12-23 21:51:56 +0530

    some bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { uploadCloudinary } from "../utils/Cloudinary.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const generateAccessAndRefreshToken = async (userID)=>{
+const generateAccessAndRefereshTokens = async (userID)=>{
     const user=await User.findById(userID);
     const accessToken = user.generateAccessToken();
     const refreshToken = user.generateRefreshToken();
@@ -61,41 +61,63 @@ export const registerUser=asyncHandler(async (req,res)=>{
     )
 })
 
-export const loginUser=asyncHandler(async (req,res)=>{
-    const {username,email,password}=req.body
-    if(!(username || email)){
-        throw new ApiError(400,"username and email does not exist");
+export const loginUser = asyncHandler(async (req, res) =>{
+    // req body -> data
+    // username or email
+    //find the user
+    //password check
+    //access and referesh token
+    //send cookie
+
+    const {email, username, password} = req.body
+    console.log(email);
+
+    // if (!username && !email) {
+    //     throw new ApiError(400, "username or email is required")
+    // }
+    
+    // Here is an alternative of above code based on logic discussed in video:
+    if (!(username || email)) {
+        throw new ApiError(400, "username or email is required")
+        
     }
-    const user=User.findOne({
-        $or:[{username},{email}]
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
     })
 
-    if(!user){
-        throw new ApiError(404,"user does not exist");
+    if (!user) {
+        throw new ApiError(404, "User does not exist")
     }
 
-    const isPasswordValid=await user.comparePassword(password);
+   const isPasswordValid = await user.isPasswordCorrect(password)
 
-    if(!isPasswordValid){
-        throw new ApiError(401,"Invalid credential");
+   if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials")
     }
-    const {accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id);
-    
-    const loggedInUser=User.findById(user._id)
-    .select("-password -refreshToken");
 
-    const options= {
+   const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
         httpOnly: true,
-        secure: true,
+        secure: true
     }
-    return res.status(200)
-    .cookie("accessToken",accessToken,options)
-    .cookie("refreshToken",refreshToken,options)
-    .json(new ApiResponse(200,{
-        user:loggedInUser , accessToken ,refreshToken
-    },
-    "User Successfully logged In"
-    ))
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged In Successfully"
+        )
+    )
 
 })
 
