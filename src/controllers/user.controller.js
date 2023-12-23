310e7fc2bbae877220656dcd66011cf10f670259
NitroRonak <ronaks0988@gmail.com>
2023-12-24 00:39:07 +0530

    update avatar controller done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -218,4 +218,30 @@ export const updateAccountDetails=asyncHandler(async(req,res)=>{
     return res
     .status(200)
     .json(new ApiResponse(200,user,"Account details updated successfully"));
+});
+
+export const updateAvatar=asyncHandler(async (req,res)=>{
+    const avatarLocalPath=req.file?.path;
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is missing");
+    }
+    const avatar=await uploadCloudinary(avatarLocalPath);
+
+    if(!avatar.url){
+        throw new ApiError(400,"Error while uploading on avatar");
+    }
+
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar:avatar.url
+            }
+        },
+        {new:true}
+    ).select("-password");
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user,"Avatar uploaded successfully"));
 })
