c2343a74914243ffbc58c6851e654fa6c3d6334c
NitroRonak <ronaks0988@gmail.com>
2023-12-26 17:48:33 +0530

    Add new routes for user controller

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { loggedOutUser, loginUser, refereshAccessToken, registerUser } from '../controllers/user.controller.js';
+import { changePassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loggedOutUser, loginUser, refereshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyUser } from '../middlewares/auth.middleware.js';
 const router=express.Router();
@@ -18,4 +18,12 @@ router.route("/register").post(upload.fields([
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyUser,loggedOutUser);
 router.route("/refresh-token").post(refereshAccessToken);
+router.route("/change-password").post(verifyUser,changePassword);
+router.route("/current-user").get(verifyUser,getCurrentUser);
+router.route("/update-account").patch(verifyUser,updateAccountDetails);
+router.route("/avatar").patch(verifyUser,upload.single("avatar"),updateUserAvatar);
+router.route("/cover-image").patch(verifyUser,upload.single("/coverImage"),updateUserCoverImage);
+router.route("/c/:username").get(verifyUser,getUserChannelProfile);
+router.route("/history").get(verifyUser,getWatchHistory);
+
 export default router;
