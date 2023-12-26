387ebd252058c0f23765aa00cf202f90f172c948
NitroRonak <ronaks0988@gmail.com>
2023-12-26 16:45:42 +0530

    Get user profile controller done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -271,3 +271,70 @@ export const updateUserCoverImage=asyncHandler(async (req,res)=>{
     .status(200)
     .json(new ApiResponse(200,user,"Cover Image uploaded successfully"));
 })
+
+export const getUserProfile=asyncHandler(async (req, res)=>{
+    const {username}=req.params;
+    if(!username?.trim()){
+        throw new ApiError(400,"Username is missing");
+    }
+    const channel=await User.aggregate([
+        {
+            $match:{
+                username:username
+            }
+        },
+        {
+            $lookup:{
+                from:"subscription",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscription",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribeTo"
+            }
+        },
+        {
+            $addFields:{
+                subscriberCount:{
+                    $size:"$subscribers"
+                },
+                channelSubscribeToCount:{
+                    $size:"$subscribeTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName:1,
+                username:1,
+                coverImage:1,
+                subscriberCount:1,
+                channelSubscribeToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                email:1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(400,"channel does not exists");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,channel[0],"channel fetched successfully"));
+})
