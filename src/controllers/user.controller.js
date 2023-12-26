64ba36ffe080f4256b01a44e45126f34fb5e7d91
NitroRonak <ronaks0988@gmail.com>
2023-12-26 14:46:20 +0530

    Update cover image controller done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -220,7 +220,7 @@ export const updateAccountDetails=asyncHandler(async(req,res)=>{
     .json(new ApiResponse(200,user,"Account details updated successfully"));
 });
 
-export const updateAvatar=asyncHandler(async (req,res)=>{
+export const updateUserAvatar=asyncHandler(async (req,res)=>{
     const avatarLocalPath=req.file?.path;
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is missing");
@@ -244,4 +244,30 @@ export const updateAvatar=asyncHandler(async (req,res)=>{
     return res
     .status(200)
     .json(new ApiResponse(200,user,"Avatar uploaded successfully"));
+})
+
+export const updateUserCoverImage=asyncHandler(async (req,res)=>{
+    const coverImageLocalPath=req.file?.path;
+    if(!coverImageLocalPath){
+        throw new ApiError(400,"Cover image file is missing");
+    }
+    const coverImage=await uploadCloudinary(coverImageLocalPath);
+
+    if(!coverImage.url){
+        throw new ApiError(400,"Error while uploading on cover image");
+    }
+
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage:coverImage.url
+            }
+        },
+        {new:true}
+    ).select("-password");
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user,"Cover Image uploaded successfully"));
 })
